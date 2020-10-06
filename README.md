332223a117ec2a920ce24336f9927d941e2f39fc
ms.seol <ms.seol@npixel.co.kr>
2020-10-06 16:07:13 +0900

    update

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ spring-webflux-reactive-redis sample
 
 webflux sample 
 
+- 기본 WebClient 사용 예, ReactiveRedis 사용 예
+
+----------------
+
 추후 살을 붙이자
--- src/main/java/com/webflux/controller/SampleController.java
+++ src/main/java/com/webflux/controller/SampleController.java
@@ -1,12 +1,14 @@
 package com.webflux.controller;
 
+import static org.springframework.web.reactive.function.BodyInserters.fromFormData; //★
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
-import org.springframework.data.redis.core.ReactiveValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.webflux.model.Test;
 
@@ -16,18 +18,45 @@ import reactor.core.publisher.Mono;
 @RestController
 public class SampleController {
 
+    // Using Webclint
+    private final WebClient webClient;
+
+    public SampleController(WebClient.Builder builder) {
+        this.webClient = builder.baseUrl("http://대상주소:9999").build();
+    }
+
+    @GetMapping("/test")
+    public Mono<String> selectGreeting(String msg) {
+        // 기타 상황에 유용한 Operator 너무많다.
+        // https://luvstudy.tistory.com/100
+
+        // Post Example
+        webClient.post()
+                 .uri("/sample")
+                 .body(fromFormData("name", "wonwoo"))
+                 .retrieve()
+                 .bodyToMono(String.class);
+
+        // Get Example
+        return webClient.get()
+                        .uri("/test/{msg}", msg)
+                        .retrieve()
+                        .bodyToMono(String.class);
+    }
+
+    // Using ReactiveRedis
     @Autowired
     private ReactiveRedisOperations<String, Test> sampleOps;
 
     @GetMapping("/test/{id}")
     public Mono<Test> selectSamples(@PathVariable String id) {
-        System.out.println("id : " + id);
         return sampleOps.opsForValue().get(id);
     }
 
     @GetMapping("/test")
     public Flux<Test> selectAllSamples() {
-        return sampleOps.keys("*").flatMap(sampleOps.opsForValue()::get);
+        return sampleOps.keys("*")
+                    .flatMap(sampleOps.opsForValue()::get);
     }
 
     @PutMapping("/test/{id}")
